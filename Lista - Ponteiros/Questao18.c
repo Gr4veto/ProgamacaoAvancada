e4c928d2b7218c6dadb4e81525c49ca2f81640b1
Wesley Costa <wesleycdoccg@gmail.com>
2022-05-12 19:42:28 -0300

    commit 15

--- Lista - Ponteiros/Questao18.c
+++ Lista - Ponteiros/Questao18.c
@@ -23,24 +23,25 @@ int i, j, k; //auxiliares
 int main (void){
 	
 	setlocale(LC_ALL,"");
+	srand(time(0));
 	
-	int **ma, **mb, **mc,
-	nlA, ncA, ncB;
+	int **ma, **mb, **mc, nlA, ncA, ncB;
 	
 	printf("Digite o numero de linhas e colunas da matriz(ex.: i j): ");
 	scanf("%d %d", &nlA, &ncA); //guardando dimensões da matriz
 	ncB = nlA;//atribuindo o numero de linhas de A ao numero de colunas de B
 	
 	//alocando matrizes dinâmicamente:
+		
 	ma = malloc(nlA*sizeof(int*));
-	ma[0] = malloc(ncA*nlA*sizeof(int));
+	ma[0] = malloc(ncA*nlA*sizeof(int*));
 	for (i = 1; i< nlA; i++){
   	ma[i] = ma[i-1] + ncA;
 	}
 	
 	mb = malloc(ncA*sizeof(int*));
 	mb[0] = malloc(ncA*ncB*sizeof(int));
-	for (i = 1; i< nlA; i++){
+	for (i = 1; i< ncA; i++){
   	mb[i] = mb[i-1] + ncB;
 	}
 	
@@ -61,7 +62,7 @@ int main (void){
 	printf("\nMatriz B:\n");
 	matrixRead(ncA, ncB, mb);
 	printf("\nMatriz C:\n");
-	matrixRead(nlA, nlA, mc);
+	matrixRead(nlA, ncB, mc);
 	
 	//Liberando memoria:
 	free(mc[0]);
@@ -78,7 +79,7 @@ void matrixWrite (int nl, int nc, int **m){
 	
 	for (i = 0; i<nl; i++){
   		for (j =0; j<nc; j++){
-    		m[i][j] = (rand()%10);
+    		m[i][j] = (rand()%5);
   		}
 	}					
 }
@@ -88,7 +89,7 @@ void matrixRead (int nl, int nc, int **m){
 	
 	for (i = 0; i < nl; i++){
   		for (j =0; j < nc; j++){
-    		printf(" %d ", m[i][j]);
+    		printf("|%d|", m[i][j]);
   		}
   		printf("\n");
 	}		
@@ -96,11 +97,23 @@ void matrixRead (int nl, int nc, int **m){
 
 //Multiplicação de matrizes:
 void matrixX(int nlA, int ncA, int ncB, int **ma, int **mb, int **mc){
+	
+	//zerando toda matriz produto:
+	for (i = 0; i<nlA; i++){
+  		for (j =0; j<ncB; j++){
+  			mc[i][j] = 0;
+		}			  
+	}
 
+	//atribuindo valor a matriz produto
 	for (i = 0; i<nlA; i++){
   		for (j =0; j<ncB; j++){
     		for(k = 0; k<ncA; k++){
-    		 mc[i][j] = ma[i][k]*mb[k][j]+ mc[i][j];
+    		 mc[i][j] = ma[i][k]*mb[k][j]+mc[i][j];
+    		 //[0][0] = ma[0][0]mb[0][0]+[0][0]
+    		 //[0][0] = ma[0][1]mb[1][0]+[0][0]
+    		 //...
+    		 //[0][0] = ma[0][n]mb[n][0]+[0][0]
     		}
   		}
 	}
